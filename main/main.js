c89e3f0d83f83afad1fdeaf33832deb504944e12
chj <389746614@qq.com>
2017-07-12 15:58:45 +0800

    change

--- main/main.js
+++ main/main.js
@@ -1,9 +1,53 @@
 'use strict';
 
 function printReceipt(inputs) {
-  console.log('请在此实现练习要求，并改写该行代码。');
-}
-function calCount (barcodeArray) {
-  var billArray=
+  var billarray=calCount (inputs);
+  calSum(billarray);
+  var totalprice=calTotalPrice(billarray);
+  printSheet(billarray,totalprice);
 
 }
+
+function calCount (barcodeArray) {
+  var billArray=loadAllItems();
+  var count=0;
+  for(var i in billArray){
+    count=0;
+    for(var j in barcodeArray){
+      if(barcodeArray[j]==billArray[i].barcode)
+        count++;
+    }
+    billArray[i].count=count;
+  }
+  return billArray;
+
+}
+function calSum(billArray) {
+  for(var i in billArray){
+    billArray[i].sum=billArray[i].price*billArray[i].count;
+  }
+  var sumArray=billArray;
+  return sumArray;
+
+}
+function calTotalPrice(sumArray) {
+  var totalPrice=0;
+  for(var i in sumArray){
+    totalPrice+=sumArray[i].sum;
+  }
+  return totalPrice;
+}
+function printSheet(sumArray,totalPrice) {
+  var str="***<没钱赚商店>收据***\n"
+  for(var i in sumArray){
+    if(sumArray[i].count!=0)
+    str+="名称："+sumArray[i].name+"，数量："+sumArray[i].count+sumArray[i].unit+"，单价："+sumArray[i].price.toFixed(2)+"(元)"+"，小计："+sumArray[i].sum.toFixed(2)+"(元)\n";
+  }
+  str+="----------------------\n";
+  str+="总计："+totalPrice.toFixed(2)+"(元)\n";
+  str+="**********************";
+  console.log(str);
+
+}
+
+
